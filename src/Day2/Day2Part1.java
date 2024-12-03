e2f8867ebbc20a44f7520877aa1d873e7043e842
moritz.ahlrichs <moritz.ahlrichs@rwth-aachen.de>
2024-12-03 14:35:11 +0100

    Day 2 Part 1 completed and accepted + Day 1 Part 2 copied from part one, level dampener noct included yet

---
+++ src/Day2/Day2Part1.java
@@ -0,0 +1,52 @@
+package Day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day2Part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/moritzahlrichs/Documents/AdventOfCode2024/src/Day2/input.txt");
+
+        Scanner sc = new Scanner(file);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        String[] parts;
+
+        int safeCounter = 0;
+        while (sc.hasNextLine()) {
+            parts = sc.nextLine().split(" ");
+            list.clear();
+            for (String part : parts) {
+                list.add(Integer.parseInt(part));
+            }
+            if (list.size() == 1) {
+                safeCounter++;
+            } else {
+                boolean safe = isSafe(list);
+                if (safe) {
+                    safeCounter++;
+                }
+            }
+        }
+
+        sc.close();
+
+        System.out.println(safeCounter);
+    }
+
+    static boolean isSafe(ArrayList<Integer> list) {
+        boolean increasing;
+        increasing = list.get(1) > list.get(0);
+        boolean safe = true;
+        for (int i = 0; i < list.size()-1; i++) {
+            if (increasing && ((list.get(i+1) <= list.get(i)) || ((list.get(i+1) - list.get(i)) > 3))) {
+                safe = false;
+            } else if (!increasing && ((list.get(i + 1) >= list.get(i)) || ((list.get(i) - list.get(i + 1)) > 3))) {
+                safe = false;
+            }
+        }
+        return safe;
+    }
+}
