b1de634273151d81a4bb5c89304a2ba77c3ce129
moritz.ahlrichs <moritz.ahlrichs@rwth-aachen.de>
2024-12-04 11:46:57 +0100

    Added Day 4 both files and Part 1 Scanner

---
+++ src/Day4/Day4Part1.java
@@ -0,0 +1,22 @@
+package Day4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+//import java.util.ArrayList;
+import java.util.Scanner;
+//import java.util.StringJoiner;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+
+public class Day4Part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/Day4/input.txt");
+        Scanner scanner = new Scanner(file);
+        String input = "";
+
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            System.out.println(input);
+        }
+    }
+}
---
+++ src/Day4/Day4Part2.java
@@ -0,0 +1,4 @@
+package Day4;
+
+public class Day4Part2 {
+}
